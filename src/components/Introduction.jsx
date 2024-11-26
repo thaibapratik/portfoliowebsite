20de426690ed30b279e4377843d755a47b262564
Pratik <bopsdlops>
2024-11-27 04:18:40 +0545

    Improved responsiveness the pervious components

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -5,17 +5,17 @@ import { FaMapPin } from "react-icons/fa";
 
 const Introduction = () => {
 	return (
-		<section className="flex items-center gap-40">
+		<section className="flex items-center gap-40 h-full">
 			<div className="mb-14">
-				<h1 className="font-bold text-4xl">
+				<h1 className="font-bold lg:text-4xl text-2xl">
 					Hello, my name is <br />
-					<span className="text-5xl">Pratik Thaiba</span>
+					<span className="lg:text-5xl text-3xl">Pratik Thaiba</span>
 				</h1>
-				<div className="flex gap-1 py-5 justify-items-center">
-					<FaMapPin size={25} />{" "}
-					<span className="text-lg">Nepal, Butwal</span>
+				<div className="flex gap-1 py-5 items-center">
+					<FaMapPin size={18} />
+					<span className="text-md">Nepal, Butwal</span>
 				</div>
-				<div className="flex gap-[7px] text-lg font-medium items-center">
+				<div className="flex gap-[7px] xl:text-lg font-medium items-center text:md">
 					<span>I'm a</span>
 					<Typewriter
 						options={{
@@ -26,7 +26,7 @@ const Introduction = () => {
 					/>
 				</div>
 			</div>
-			<div className="h-[450px] w-[350px]">
+			<div className="h-[300px] w-[200px] xl:h-[350px] xl:w-[250px]">
 				<img
 					src={profile}
 					alt="profile"
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import "../mesh.css";
 const NavBar = ({ blurColor }) => {
 	return (
 		<section
-			className={`flex justify-center items-center gap-10 text-white bg-transparent border-1 border-black/50 m-5 p-2 rounded-full w-fit text-center font-semibold backdrop-blur-md fixed top-0`}
+			className={`flex justify-center items-center gap-10 text-white bg-transparent border-1 border-black/50 m-5 p-2 rounded-full w-fit text-center font-semibold backdrop-blur-md fixed top-0 max-lg:p-1`}
 		>
 			<a href="" className="nav-links">
 				Home
